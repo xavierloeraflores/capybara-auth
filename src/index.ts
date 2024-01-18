933445875306dbb50923c16a129724d0b264b743
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-18 01:22:10 -0800

    Express server closes #6

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import {Request, Response} from "express";
+const express = require('express');
+const port = 3000;
+
 console.log('Hello World!');
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({message: 'Hello World!'});
+});
+
+app.listen(port, () => {    
+  console.log(`Listening on port ${port}`);
+});
