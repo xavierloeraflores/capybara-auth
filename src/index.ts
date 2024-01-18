fe2b7dc3321d39accc0ca99f11c8505637bac520
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-18 15:07:26 -0800

    Error handling + 404 err closes #23

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 require ('dotenv').config();
-import {Request, Response} from "express";
+import {Request, Response, NextFunction, ErrorRequestHandler,Errback } from "express";
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
@@ -25,6 +25,32 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/auth', router);
+
+class ExpressError extends Error {
+  status: number; 
+  constructor(message: string, status: number) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
+class NotFoundError extends ExpressError {
+  constructor(message = "Not Found") {
+    super(message, 404);
+  }
+}
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  return next(new NotFoundError());
+});
+
+//Function Type:ErrorRequestHandler
+app.use(function (err: ExpressError, req: Request, res: Response, next: NextFunction) {
+  const status = err.status || 500;
+  const message = err.message;
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
 
 app.listen(port, () => {    
   console.log(`Listening on port ${port}`);
