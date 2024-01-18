e118afc4aae0be758b489ba584d85397d7964479
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-18 13:09:24 -0800

    Made PORT an env variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 require ('dotenv').config();
 import {Request, Response} from "express";
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 console.log('Hello World!');
 
