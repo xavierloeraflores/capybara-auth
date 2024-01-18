3ab6236b8468425ecd26e5076c35e871f977c3af
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-18 13:32:24 -0800

    DB Connection closes #22

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 require ('dotenv').config();
 import {Request, Response} from "express";
 const express = require('express');
-const port = process.env.PORT || 3000;
-
-console.log('Hello World!');
-
 const app = express();
+const mysql = require('mysql');
+
+
+const port = process.env.PORT || 3000;
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect((err: any) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+
 
 app.get('/', (req: Request, res: Response) => {
-  res.send({message: 'Hello World!'});
+  connection.query('SELECT * from test_test', function (error: Error, results: any[], _fields: any[]) {
+    if (error) {
+      res.send({message: 'Could not connect to database'});
+      throw error;
+    }
+    res.send(results[0]);
+  });
 });
 
 app.listen(port, () => {    
